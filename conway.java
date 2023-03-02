f2d323038ea91cfc3635b99de709090cea6d9580
Asaph_IF <98226046+AsaphIF@users.noreply.github.com>
2023-03-02 08:56:24 -0300

    arquivo java primeiro teste
    ele tem as regras e printa 1 geração
    preciso colocar para atualizar sempre

---
+++ conway.java
@@ -0,0 +1,85 @@
+public class myconway {
+    
+    public static void prox(int grid[][], int m , int n){
+        int[][] futuro = new int[m][n];
+
+        for(int l = 0; l < m; l++)
+        {
+            for(int p = 0; p < m; p++)
+            {
+                int alive = 0;
+                for (int i = -1; i <= 1; i++)
+                    for(int j = -1; j <= 1; j++)
+                        if((l+i>=0&&l+i<m)&&(p+j>=0&&p+j<n))
+                         alive += grid[l+i][p+j];
+                
+                alive -=grid[l][p];
+
+                if((grid[l][p]==1)&&(alive < 2))
+                    futuro[l][p] = 0; 
+
+                if((grid[l][p]==1)&&(alive > 3))
+                    futuro[l][p] = 0; 
+
+                if((grid[l][p]==0)&&(alive == 3))
+                    futuro[l][p] = 1; 
+                else 
+                    futuro[l][p] = grid[l][p];
+
+            }
+        }
+
+        System.out.println("novo");
+
+        for (int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if (futuro[i][j] == 0 )
+                    System.out.print("_");
+                else
+                    System.out.print("#");            
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+
+
+
+
+
+    }
+    
+    public static void main(String[] args){
+
+        int m = 10, n = 10 ;
+
+        int[][] grid = {    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                            { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                            { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                            { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+                            { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+                            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+                        };
+
+        for (int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if (grid[i][j] == 0 )
+                    System.out.print("_");
+                else
+                    System.out.print("#");            
+            }
+            System.out.println();
+        }
+        System.out.println();
+        prox(grid, m , n);
+
+    }
+
+
+
+}
