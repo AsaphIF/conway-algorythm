f07798e2506d879ea876c9d56fab00a00fdbc979
Asaph_IF <98226046+AsaphIF@users.noreply.github.com>
2023-03-22 09:35:12 -0300

    2 grades e mapa infinito

--- conway.java
+++ conway.java
@@ -2,9 +2,9 @@ package conway;
 
 public class Conway {
     // quantas vezes o loop se repete
-    public static int roda = 20;
+    public static int roda = 150;
     // tamanho da grade
-    public static int m = 20, n = 20 ;
+    public static int maxLinha = 20, maxColuna = 20 ;
     //padrão padrão
     public static int grade[][] =    {      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
                                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -22,6 +22,27 @@ public class Conway {
                                             { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0 },
                                             { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
                                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+                                    };
+    public static int grade2[][] =    {     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+                                            { 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+                                            { 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0 },
+                                            { 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+                                            { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
                                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
                                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
                                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -60,39 +81,64 @@ public class Conway {
         //proxima grade
         int[][] futuro = new int[m][n];
         //linha da grid
-        for(int l = 0; l < m; l++)
+        for(int glinha = 0; glinha < m; glinha++)
         {
             //coluna da grid
-            for(int p = 0; p < m; p++)
+            for(int gcoluna = 0; gcoluna < n; gcoluna++)
             {
                 //variavel que verifica quantas celulas estão vivas
                 int alive = 0;
                 //linha
-                for (int i = -1; i <= 1; i++)
+                for (int alinha = -1; alinha <= 1; alinha++)
                     //coluna
-                    for(int j = -1; j <= 1; j++)
+                    for(int acoluna = -1; acoluna <= 1; acoluna++){
                         //se tiver uma celula dentro da grid
-                        if((l+i>=0&&l+i<m)&&(p+j>=0&&p+j<n))
+                        if(( glinha + alinha >= 0 && glinha + alinha < m )&&( gcoluna + acoluna >= 0 && gcoluna + acoluna < n))
                             //conta a celula
-                         alive += grid[l+i][p+j];
-                            
+                         alive += grid[glinha+alinha][gcoluna+acoluna];
+                        
+                        else if(( glinha + alinha >= 0 && glinha + alinha < m )&& gcoluna + acoluna >= n)
+                            alive += grid[glinha+alinha][0];
+                        
+                        else if(( glinha + alinha >= 0 && glinha + alinha < m )&& gcoluna + acoluna <= -1)
+                            alive += grid[glinha+alinha][n-1];
+                        
+                        else if( glinha + alinha <= -1 && ( gcoluna + acoluna >= 0 && gcoluna + acoluna < n))
+                            alive += grid[m-1][gcoluna+acoluna];
+                        
+                        else if( glinha + alinha >= m && ( gcoluna + acoluna >= 0 && gcoluna + acoluna < n))
+                            alive += grid[0][gcoluna+acoluna];
+                        
+                        else if( glinha + alinha >= m && gcoluna + acoluna <= -1)
+                            alive += grid[0][n-1];
+                        
+                        else if( glinha + alinha >= m && gcoluna + acoluna >= n)
+                            alive += grid[0][0];
+                        
+                        else if( glinha + alinha <= -1 && gcoluna + acoluna <= -1)
+                            alive += grid[m-1][n-1];
+                        
+                        else if( glinha + alinha <= -1 && gcoluna + acoluna >= n)
+                            alive += grid[m-1][0];
+                         
+                    }     
                 //tira a celula lida
-                alive -= grid[l][p];
+                alive -= grid[glinha][gcoluna];
                 
                 //regras
                 
                 //Qualquer célula viva com menos de dois vizinhos vivos morre como se causada por subpopulação. 
-                if( ( grid[l][p] == 1 ) && ( alive < 2 ) )
-                    futuro[l][p] = 0; 
+                if( ( grid[glinha][gcoluna] == 1 ) && ( alive < 2 ) )
+                    futuro[glinha][gcoluna] = 0; 
                 //Qualquer célula viva com mais de três vizinhos vivos morre, como se por superpopulação.
-                else if( ( grid[l][p] == 1 ) && ( alive > 3 ) )
-                    futuro[l][p] = 0; 
+                else if( ( grid[glinha][gcoluna] == 1 ) && ( alive > 3 ) )
+                    futuro[glinha][gcoluna] = 0; 
                 //Qualquer célula morta com exatamente três vizinhos vivos torna-se uma célula viva, como se por reprodução.
-                else if( ( grid[l][p] == 0 ) && ( alive == 3 ) )
-                    futuro[l][p] = 1; 
+                else if( ( grid[glinha][gcoluna] == 0 ) && ( alive == 3 ) )
+                    futuro[glinha][gcoluna] = 1; 
                 //Qualquer célula viva com dois ou três vizinhos vivos passa para a próxima geração.
                 else 
-                    futuro[l][p] = grid[l][p];
+                    futuro[glinha][gcoluna] = grid[glinha][gcoluna];
 
             }
         }
@@ -102,16 +148,16 @@ public class Conway {
     
     public static void main(String[] args) throws InterruptedException{
         
-        int grid[][] =  grade;
-        print(grid,m,n);
-            Thread.sleep(150);
-            cls();
+        int grid[][] =  grade2;
+        print(grid,maxLinha,maxColuna);
+        Thread.sleep(100);
+        cls();
         for (int i = 0; i < roda; i++){
             
-            grid = prox(grid, m , n);
+            grid = prox(grid, maxLinha , maxColuna);
             System.out.println("");
-            print(grid,m,n);
-            Thread.sleep(150);
+            print(grid,maxLinha,maxColuna);
+            Thread.sleep(100);
             cls();
             
            
